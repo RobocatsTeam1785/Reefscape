d7706a7731948550083822fa688aca67269afec0
Nyla <nfluharty@proton.me>
2025-02-13 17:28:38 -0600

    refactor(controller): switch to an abstracted mode state system style(controller): eliminate alignEnabled parameter from driveAlign and driveOnly to be consistent with an explicitly function-local controller usage configuration model

---
+++ src/main/java/frc/lib/mode/ModeState.java
@@ -0,0 +1,85 @@
+package frc.lib.mode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SelectCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class ModeState<M> {
+    /** cache to store triggers comparing current and possible modes, to avoid creating an arbitrary number of redundant Triggers */
+    private final HashMap<M, Trigger> isCache = new HashMap<>();
+
+    /** list of mode switch triggers */
+    private final ArrayList<Trigger> switches = new ArrayList<>();
+
+    /** whether all mode switch triggers are inactive */
+    private final Trigger noSwitchesActive = new Trigger(() -> switches.stream().allMatch(t -> !t.getAsBoolean()));
+
+    /** the mode the controller uses at startup */
+    public final M defaultMode;
+
+    /** the current mode the controller is using */
+    private M currentMode;
+
+    public ModeState(M defaultMode) {
+        this.defaultMode = defaultMode;
+        currentMode = defaultMode;
+    }
+
+    // mode
+    /** returns the current mode */
+    public M mode() {
+        return currentMode;
+    }
+
+    // switching
+    /** registers a mode switch that sets the current mode to the specified mode when the trigger becomes true */
+    public void registerSwitch(M mode, Trigger trigger) {
+        // record the switch
+        switches.add(trigger);
+
+        // change the mode when the trigger becomes true
+        trigger.onTrue(new InstantCommand(() -> {
+            currentMode = mode;
+        }));
+    }
+
+    // triggers
+    /** returns a trigger describing whether the current mode equals the specified mode */
+    public Trigger is(M mode) {
+        return isCache.putIfAbsent(mode, new Trigger(() -> currentMode == mode()));
+    }
+
+    /** whether all mode switch triggers are inactive */
+    public Trigger noSwitchesActive() {
+        return noSwitchesActive;
+    }
+
+    // commands
+    /** returns a command that selects between the given command map based on the current mode */
+    public SelectCommand<M> select(Map<M, Command> commands) {
+        return new SelectCommand<>(commands, this::mode);
+    }
+
+    /** returns a command that selects between the given runnable map based on the current mode, with each runnable having the specified requirements */
+    public SelectCommand<M> selectRunnable(Map<M, Runnable> runnables, Subsystem... requirements) {
+        // get the entries, convert the set to a stream, replace each runnable with an instant command that runs it,
+        // and collect that stream of entries back into a map of modes and commands
+        Map<M, Command> commands = runnables
+            .entrySet()
+            .stream()
+            .map(e -> Map.entry(
+                e.getKey(),
+                new InstantCommand(e.getValue(), requirements)
+            ))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        
+        return new SelectCommand<>(commands, this::mode);
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,11 +2,13 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.Radians;
 
+import java.util.Map;
+
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.lib.utility.CommandUtils;
+import frc.lib.mode.ModeState;
 import frc.robot.modes.DriveMode;
 import frc.robot.subsystems.Drive;
 
@@ -15,7 +17,7 @@ public class RobotContainer {
     private final Drive drive;
 
     // modes
-    private DriveMode mode = DriveMode.ALIGN;
+    private ModeState<DriveMode> driveState = new ModeState<>(DriveMode.ALIGN);
 
     // controllers use NED CCC (https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html)
     private final CommandXboxController
@@ -31,34 +33,20 @@ public class RobotContainer {
     private void configureCommands() {
         // buttons
         // d-pad up: a is swerve, and b is align
-        driver.povUp().and(driver.a()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.SWERVE;
-        }));
-
-        driver.povUp().and(driver.b()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.ALIGN;
-        }));
-
+        driveState.registerSwitch(DriveMode.SWERVE, driver.povUp().and(driver.a()));
+        driveState.registerSwitch(DriveMode.ALIGN, driver.povUp().and(driver.b()));
+        
         // d-pad right: y is FL, b is FR, x is BL, a is BR, so normal directions but rotated 45 degrees CW
-        driver.povRight().and(driver.y()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.FL_ONLY;
-        }));
-
-        driver.povRight().and(driver.b()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.FR_ONLY;
-        }));
-
-        driver.povRight().and(driver.x()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.BL_ONLY;
-        }));
-
-        driver.povRight().and(driver.a()).onTrue(new InstantCommand(() -> {
-            mode = DriveMode.BR_ONLY;
-        }));
+        driveState.registerSwitch(DriveMode.FL_ONLY, driver.povRight().and(driver.y()));
+        driveState.registerSwitch(DriveMode.FR_ONLY, driver.povRight().and(driver.b()));
+        driveState.registerSwitch(DriveMode.BL_ONLY, driver.povRight().and(driver.x()));
+        driveState.registerSwitch(DriveMode.BR_ONLY, driver.povRight().and(driver.a()));
 
         // when a is pressed and in a single-module-only mode, zero the relative turn encoder
         // d-pad is negated to avoid collision with mode-switching
-        driver.a().and(driver.povUp().negate()).and(driver.povRight().negate()).onTrue(new InstantCommand(() -> {
+        driver.a().and(driveState.noSwitchesActive()).onTrue(new InstantCommand(() -> {
+            DriveMode mode = driveState.mode();
+
             if (mode.oneModuleOnly()) {
                 drive.zeroRelTurnEncoder(mode.id);
             }
@@ -66,6 +54,8 @@ public class RobotContainer {
 
         // zero turn voltage when the right trigger is lifted
         driver.rightTrigger(0.5).negate().onTrue(new InstantCommand(() -> {
+            DriveMode mode = driveState.mode();
+
             if (mode.oneModuleOnly()) {
                 drive.zeroTurnVoltage(mode.id);
             } else if (mode == DriveMode.ALIGN) {
@@ -74,19 +64,14 @@ public class RobotContainer {
         }));
 
         // default commands
-        CommandUtils.setDefault(drive, () -> {
-            // only align when the right trigger is held down
-            boolean alignEnabled = driver.getRightTriggerAxis() > 0.5;
-
-            switch(mode) {
-                case SWERVE -> driveSwerve();
-                case ALIGN -> driveAlign(alignEnabled);
-                case FL_ONLY -> driveOnly(0, alignEnabled);
-                case FR_ONLY -> driveOnly(1, alignEnabled);
-                case BL_ONLY -> driveOnly(2, alignEnabled);
-                case BR_ONLY -> driveOnly(3, alignEnabled);
-            }
-        });
+        drive.setDefaultCommand(driveState.selectRunnable(Map.of(
+            DriveMode.SWERVE, this::driveSwerve,
+            DriveMode.ALIGN, this::driveAlign,
+            DriveMode.FL_ONLY, () -> driveOnly(0),
+            DriveMode.FR_ONLY, () -> driveOnly(1),
+            DriveMode.BL_ONLY, () -> driveOnly(2),
+            DriveMode.BR_ONLY, () -> driveOnly(3)
+        ), drive));
     }
 
     // periodic
@@ -94,7 +79,7 @@ public class RobotContainer {
         // dashboard
         drive.updateDashboardValues();
 
-        SmartDashboard.putString("drive mode", mode.toString());
+        SmartDashboard.putString("drive mode", driveState.mode().toString());
     }
 
     // controller values
@@ -136,7 +121,6 @@ public class RobotContainer {
     /** drives in swerve mode using the left joystick for translation and the right joystick for rotation */
     public void driveSwerve() {
         // convert X and Y rotation from NED CCC to X and Y coordinates in ENU CCC
-        
         // fully right means 1, which is intuitive
         double xSpeed = driver.getLeftX();
 
@@ -153,20 +137,25 @@ public class RobotContainer {
     /**
      * aligns to the rotation designated by the driver left joystick, where fully right means zero, and counterclockwise is positive
      * in turn, "zero" is defined as the yaw the robot had when the navX was last zeroed
+     * 
+     * alignment only occurs when the right trigger is down
      */
-    public void driveAlign(boolean alignEnabled) {
-        if (alignEnabled) {
-            drive.align(leftAlignAngle());
-        }
+    public void driveAlign() {
+        if (driver.getRightTriggerAxis() <= 0.5) return;
+
+        drive.align(leftAlignAngle());
     }
 
     /**
      * drives a single swerve module using the left joystick (up and down meaning forward and backward) and the right joystick to control angle,
      * where the id is in [0, 3], in the order FL, FR, BL, BR
+     * 
+     * alignment only occurs when the right trigger is down
      */
-    public void driveOnly(int moduleId, boolean alignEnabled) {
-        // convert X and Y rotation from NED CCC to X and Y coordinates in ENU CCC
+    public void driveOnly(int moduleId) {
+        boolean alignEnabled = driver.getRightTriggerAxis() > 0.5;
 
+        // convert X and Y rotation from NED CCC to X and Y coordinates in ENU CCC
         // fully up means -1, which is unintuitive in ENU, so it requires inversion
         double speed = -driver.getLeftY();
 
