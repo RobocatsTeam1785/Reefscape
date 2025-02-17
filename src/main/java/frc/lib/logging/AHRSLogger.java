717a2881a65f2f524600da7fa0442616063e4cc5
Nyla Fluharty <nfluharty@proton.me>
2025-02-17 10:49:42 -0600

    feat(logging): add AHRS logger, with continous yaw, yaw, and heading

---
+++ src/main/java/frc/lib/logging/AHRSLogger.java
@@ -0,0 +1,21 @@
+package frc.lib.logging;
+
+import com.studica.frc.AHRS;
+
+import edu.wpi.first.epilogue.CustomLoggerFor;
+import edu.wpi.first.epilogue.logging.ClassSpecificLogger;
+import edu.wpi.first.epilogue.logging.EpilogueBackend;
+
+@CustomLoggerFor(AHRS.class)
+public class AHRSLogger extends ClassSpecificLogger<AHRS> {
+    public AHRSLogger() {
+        super(AHRS.class);
+    }
+
+    @Override
+    public void update(EpilogueBackend backend, AHRS ahrs) {
+        backend.log("Continuous yaw (degrees)", ahrs.getAngle());
+        backend.log("Yaw (degrees)", ahrs.getYaw());
+        backend.log("Heading (degrees)", ahrs.getFusedHeading());
+    }
+}
