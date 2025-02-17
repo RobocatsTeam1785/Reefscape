c26c533d6c611e22e6f360b34cceadcc64522712
Nyla Fluharty <nfluharty@proton.me>
2025-02-17 10:49:37 -0600

    docs(constants): correct description of RobotConstants#TUNING

--- src/main/java/frc/lib/constants/RobotConstants.java
+++ src/main/java/frc/lib/constants/RobotConstants.java
@@ -2,6 +2,6 @@ package frc.lib.constants;
 
 /** constants that apply to the entire robot */
 public class RobotConstants {
-    /** whether the robot is being PID tuned - used for checking whether to automatically update PID values */
+    /** whether the robot is being tuned - used for checking whether to automatically update PID and FF gains */
     public final static boolean TUNING = true;
 }
