00edeefc4aa49006bd79e0476e943743d103b23f
Nyla Fluharty <nfluharty@proton.me>
2025-02-17 10:49:36 -0600

    refactor(controller): switch to an input-processor-based system refactor(controller): switch to using epilogue for logging drive modes

--- src/main/java/frc/lib/mode/ModeState.java
+++ src/main/java/frc/lib/mode/ModeState.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
@@ -25,7 +26,7 @@ public class ModeState<M> {
     public final M defaultMode;
 
     /** the current mode the controller is using */
-    private M currentMode;
+    @Logged private M currentMode;
 
     public ModeState(M defaultMode) {
         this.defaultMode = defaultMode;
---
+++ src/main/java/frc/robot/input/InputProcessor.java
@@ -0,0 +1,46 @@
+package frc.robot.input;
+
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+/** an object that transforms controller inputs into subsystem setpoints */
+public abstract class InputProcessor {
+    public abstract void configureProcessing();
+
+    // utility functions
+    /**
+     * the rotation designated by the driver left joystick, where fully right means zero, and counterclockwise is positive
+     * in turn, "zero" is defined as the yaw the robot had when the navX was last zeroed
+     */
+    public static Angle leftAlignAngle(CommandXboxController driver) {
+        // convert X and Y rotation from NED CCC to X and Y coordinates in ENU CCC
+
+        // fully right means 1, which is intuitive in ENU
+        double x = driver.getLeftX();
+
+        // fully up means -1, which is unintuitive in ENU, so it requires inversion
+        double y = -driver.getLeftY();
+
+        double angleRadians = Math.atan2(y, x);
+        return Radians.of(angleRadians);
+    }
+
+    /**
+     * the rotation designated by the driver right joystick, where fully right means zero, and counterclockwise is positive
+     * in turn, "zero" is defined as the yaw the robot had when the navX was last zeroed
+     */
+    public static Angle rightAlignAngle(CommandXboxController driver) {
+        // convert X and Y rotation from NED CCC to X and Y coordinates in ENU CCC
+
+        // fully right means 1, which is intuitive in ENU
+        double x = driver.getRightX();
+
+        // fully up means -1, which is unintuitive in ENU, so it requires inversion
+        double y = -driver.getRightY();
+
+        double angleRadians = Math.atan2(y, x);
+        return Radians.of(angleRadians);
+    }
+}
