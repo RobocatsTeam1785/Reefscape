91561fbd819960e8e5170493548f91e7034e931e
Nyla Fluharty <nfluharty@proton.me>
2025-02-17 10:49:37 -0600

    fix(constants): eliminate now-unused DEBUGGING constant

--- src/main/java/frc/lib/constants/RobotConstants.java
+++ src/main/java/frc/lib/constants/RobotConstants.java
@@ -2,9 +2,6 @@ package frc.lib.constants;
 
 /** constants that apply to the entire robot */
 public class RobotConstants {
-    /** whether or not the robot is in debug mode - used for checking whether to log */
-    public final static boolean DEBUGGING = true;
-
     /** whether the robot is being PID tuned - used for checking whether to automatically update PID values */
     public final static boolean TUNING = true;
 }
