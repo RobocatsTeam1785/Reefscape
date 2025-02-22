9e8fd8c421e2cbff6499e4cce74edfe0e01d2f72
Nyla Fluharty <nfluharty@proton.me>
2025-02-22 09:19:02 -0600

    fix(container): add elevator to RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,12 +3,14 @@ package frc.robot;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.input.SubsystemInputProcessor;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Swerve;
 
 @Logged
 public class RobotContainer {
     // subsystems
     private final Swerve swerve;
+    private final Elevator elevator;
 
     // controllers use NED CCC (https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html)
     private final CommandXboxController
@@ -20,9 +22,10 @@ public class RobotContainer {
     public RobotContainer(double period) {
         // subsystems
         swerve = new Swerve(period);
+        elevator = new Elevator();
 
         // processors
-        processor = new SubsystemInputProcessor(swerve, driver);
+        processor = new SubsystemInputProcessor(swerve, elevator, driver);
         processor.configure();
     }
 }
