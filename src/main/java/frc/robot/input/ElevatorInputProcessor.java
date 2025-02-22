0115194cac7c201d28e582451e9f651e17eccb04
Nyla Fluharty <nfluharty@proton.me>
2025-02-22 08:57:42 -0600

    refactor(elevator): implement an elevator input processor for manual controller speed control

---
+++ src/main/java/frc/robot/input/ElevatorInputProcessor.java
@@ -0,0 +1,65 @@
+package frc.robot.input;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.lib.constants.ElevatorConstants;
+import frc.lib.input.InputProcessor;
+import frc.lib.mode.ModeState;
+import frc.robot.modes.ElevatorMode;
+import frc.robot.subsystems.Elevator;
+
+public class ElevatorInputProcessor extends InputProcessor {
+    // subsystems
+    private final Elevator elevator;
+
+    // controllers
+    private final CommandXboxController driver;
+
+    // modes
+    private final ModeState<ElevatorMode> state;
+
+    // TODO make a read-only version of ModeState to disallow registering mode switches in an InputProcessor, outside of SubsystemInputProcessor
+    public ElevatorInputProcessor(final Elevator elevator, final CommandXboxController driver, final ModeState<ElevatorMode> state) {
+        this.elevator = elevator;
+        this.driver = driver;
+        this.state = state;
+    }
+
+    @Override
+    public void configureTriggers() {
+        // buttons
+        // state-based
+    }
+
+    @Override
+    public void configureDefaults(Map<Subsystem, Map<ModeState<?>, Command>> defaults) {
+        Map<ModeState<?>, Command> commands = defaults.putIfAbsent(elevator, new HashMap<>());
+
+        commands.put(state, state.selectRunnable(Map.of(
+            ElevatorMode.MANUAL, this::driveManual
+        ), elevator));
+    }
+
+    // driving
+    /** moves the elevator up and down using the left joystick, where upwards joystick movement means upwards elevator movement and vice versa */
+    public void driveManual() {
+        // determine input values
+        // fully up means -1, which is unintuitive, so it requires inversion
+        double controllerVerticalSpeed = -driver.getLeftY();
+
+        // process them
+        controllerVerticalSpeed = MathUtil.applyDeadband(controllerVerticalSpeed, ElevatorConstants.SPEED_DEADBAND);
+
+        // apply units
+        LinearVelocity verticalSpeed = ElevatorConstants.MAX_SPEED.times(controllerVerticalSpeed);
+
+        // drive
+        elevator.updateSetpoint(verticalSpeed);
+    }
+}
