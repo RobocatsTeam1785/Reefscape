bec8924e3d8d2202a0d3aad24844c86ce7ab9e6e
Nyla Fluharty <nfluharty@proton.me>
2025-02-22 07:44:58 -0600

    refactor(elevator): implement elevator mode

---
+++ src/main/java/frc/robot/modes/ElevatorMode.java
@@ -0,0 +1,10 @@
+package frc.robot.modes;
+
+import edu.wpi.first.epilogue.Logged;
+import frc.lib.mode.Mode;
+
+@Logged
+public enum ElevatorMode implements Mode {
+    /** default left joystick controlled up and down movement */
+    MANUAL;
+}
