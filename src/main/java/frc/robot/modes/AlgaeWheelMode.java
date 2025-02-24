e415be4cfbaa31e9d0f98c06647951a1ca2b25c7
Nyla Fluharty <nfluharty@proton.me>
2025-02-23 19:35:33 -0600

    docs(wheel): correct specs to include x to apply changes

--- src/main/java/frc/robot/modes/AlgaeWheelMode.java
+++ src/main/java/frc/robot/modes/AlgaeWheelMode.java
@@ -8,21 +8,21 @@ public enum AlgaeWheelMode implements Mode {
     /**
      * pressing the b button sets the velocity of both wheels to a constant value that can be hot-reloaded for tuning
      * <p>
-     * pressing the y button increments the speed by 0.1 m/s, and pressing the a button decrements it by 0.1 m/s
+     * pressing the y button increments the speed by 0.1 m/s, pressing the a button decrements it by 0.1 m/s, and pressing x applies the change
      */
     MANUAL,
 
     /**
      * pressing the b button sets the velocity of the left wheel to a constant value that can be hot-reloaded for tuning
      * <p>
-     * pressing the y button increments the speed by 0.1 m/s, and pressing the a button decrements it by 0.1 m/s
+     * pressing the y button increments the speed by 0.1 m/s, pressing the a button decrements it by 0.1 m/s, and pressing x applies the change
      */
     MANUAL_LEFT_ONLY,
 
     /**
      * pressing the b button sets the velocity of the right wheel to a constant value that can be hot-reloaded for tuning
      * <p>
-     * pressing the y button increments the speed by 0.1 m/s, and pressing the a button decrements it by 0.1 m/s
+     * pressing the y button increments the speed by 0.1 m/s, pressing the a button decrements it by 0.1 m/s, and pressing x applies the change
      */
     MANUAL_RIGHT_ONLY;
 }
--- src/main/java/frc/robot/modes/CoralWheelMode.java
+++ src/main/java/frc/robot/modes/CoralWheelMode.java
@@ -8,7 +8,7 @@ public enum CoralWheelMode implements Mode {
     /**
      * pressing the b button sets the velocity to a constant value that can be hot-reloaded for tuning
      * <p>
-     * pressing the y button increments the speed by 0.1 m/s, and pressing the a button decrements it by 0.1 m/s
+     * pressing the y button increments the speed by 0.1 m/s, pressing the a button decrements it by 0.1 m/s, and pressing x applies the change
      */
     MANUAL;
 }
