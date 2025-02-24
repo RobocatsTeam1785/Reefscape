635579c700639c99de7c1ebb2ce07734666d3f03
Nyla Fluharty <nfluharty@proton.me>
2025-02-23 20:03:32 -0600

    style(container): remove unnecessary final modifier and include new subsystems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,34 +1,51 @@
 package frc.robot;
 
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+// import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.input.SubsystemInputProcessor;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.Swerve;
+// import frc.robot.subsystems.AlgaeArm;
+// import frc.robot.subsystems.AlgaeWheel;
+// import frc.robot.subsystems.CoralArm;
+// import frc.robot.subsystems.CoralWheel;
+// import frc.robot.subsystems.Elevator;
+// import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Vision;
 
 @Logged(strategy = Logged.Strategy.OPT_IN)
 public class RobotContainer {
     // subsystems
-    // @Logged private final Swerve swerve;
-    // @Logged private final Elevator elevator;
-    @Logged private final Vision vision;
+    // @Logged private Swerve swerve;
+    // @Logged private Elevator elevator;
+
+    // @Logged private CoralArm coralArm;
+    // @Logged private CoralWheel coralWheel;
+
+    // @Logged private AlgaeArm algaeArm;
+    // @Logged private AlgaeWheel algaeWheel;
+
+    @Logged private Vision vision;
 
     // controllers use NED CCC (https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html)
-    private final CommandXboxController
-        driver = new CommandXboxController(0);
+    // private final CommandXboxController driver = new CommandXboxController(0);
     
     // input processors
-    // @Logged private final SubsystemInputProcessor processor;
+    @Logged private SubsystemInputProcessor processor;
 
     public RobotContainer(double period) {
         // subsystems
         // swerve = new Swerve(period);
         // elevator = new Elevator();
+
+        // coralArm = new CoralArm();
+        // coralWheel = new CoralWheel();
+
+        // algaeArm = new AlgaeArm();
+        // algaeWheel = new AlgaeWheel();
+
         vision = new Vision();
 
         // processors
-        // processor = new SubsystemInputProcessor(swerve, elevator, driver);
+        // processor = new SubsystemInputProcessor(swerve, elevator, coralArm, coralWheel, algaeArm, algaeWheel, driver);
         // processor.configure();
     }
 
