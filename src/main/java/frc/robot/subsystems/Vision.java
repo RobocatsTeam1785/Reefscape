784c7196a1096abb056597b7d7d00d18f8bc4e2e
Nyla Fluharty <nfluharty@proton.me>
2025-02-24 22:14:00 -0600

    feat(vision): implement vision pose estimation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -3,14 +3,12 @@ package frc.robot.subsystems;
 import java.util.List;
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
-
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.net.PortForwarder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.constants.VisionConstants;
@@ -24,7 +22,7 @@ public class Vision extends SubsystemBase {
     private final PhotonPoseEstimator estimator;
 
     // state
-    private List<PhotonPipelineResult> unreadResults;
+    private PhotonPipelineResult latestResult;
 
     // initialization
     public Vision() {
@@ -43,44 +41,22 @@ public class Vision extends SubsystemBase {
         PortForwarder.add(5800, "10.17.85.11", 5800);
         
         camera.setPipelineIndex(VisionConstants.PIPELINE);
-
-        // basic state hydration
-        updateResults();
-    }
-
-    // state modification
-    /** updates the unread pipeline results */
-    private void updateResults() {
-        unreadResults = camera.getAllUnreadResults();
     }
 
     // state
-    /** returns the latest unread pipeline result, or null if unreadResults is empty */
-    @Logged
-    public PhotonPipelineResult latestResult() {
-        int index = unreadResults.size() - 1;
-        return index >= 0 ? unreadResults.get(index) : null;
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d previousEstimatedRobotPose) {
+        estimator.setReferencePose(previousEstimatedRobotPose);
+        return estimator.update(latestResult);
     }
 
+    // periodic
     @Override
     public void periodic() {
-        updateResults();
-
+        // update latest result
+        List<PhotonPipelineResult> unreadResults = camera.getAllUnreadResults();
+        
         if (unreadResults.size() > 0) {
-            PhotonPipelineResult result = latestResult();
-            
-            if (result.hasTargets()) {
-                PhotonTrackedTarget target = result.getBestTarget();
-                Optional<Pose3d> targetPose = VisionConstants.COMPETITION_LAYOUT.getTagPose(target.getFiducialId());
-
-                if (targetPose.isPresent()) {
-                    Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(
-                        target.getBestCameraToTarget(),
-                        targetPose.get(),
-                        VisionConstants.CAMERA_TO_ROBOT
-                    );
-                }
-            }
+            latestResult = unreadResults.get(unreadResults.size() - 1);
         }
     }
 }
