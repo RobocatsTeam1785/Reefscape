943cbe2b4a16dae8f08b842805c6c6816eb88186
Nyla Fluharty <nfluharty@proton.me>
2025-02-28 14:26:08 -0600

    refactor(input): add JoystickModule control for elevator

--- src/main/java/frc/robot/input/ElevatorInputProcessor.java
+++ src/main/java/frc/robot/input/ElevatorInputProcessor.java
@@ -1,5 +1,9 @@
 package frc.robot.input;
 
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BooleanSupplier;
@@ -12,6 +16,9 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.constants.ElevatorConstants;
 import frc.lib.input.InputProcessor;
+import frc.lib.input.module.ControlModule;
+import frc.lib.input.module.JoystickModule;
+import frc.lib.input.module.JoystickModuleParams;
 import frc.lib.mode.ModeState;
 import frc.robot.modes.ElevatorMode;
 import frc.robot.subsystems.Elevator;
@@ -26,6 +33,20 @@ public class ElevatorInputProcessor extends InputProcessor {
     // modes
     private final ModeState<ElevatorMode> state;
 
+    // control
+    private final JoystickModuleParams defaultParams;
+
+    private final JoystickModule positionModule;
+    private final JoystickModule velocityModule;
+    private final JoystickModule voltageModule;
+
+    /** if JOYSTICK_DEADBAND is x, then controller joystick values in the range [-x, x] get reduced to zero */
+    private static final double JOYSTICK_DEADBAND = 0.15;
+
+    private static final double BUTTON_POSITION_RESET_METERS = 0.0;
+    private static final double BUTTON_VELOCITY_RESET_VOLTS = 0.0;
+    private static final double BUTTON_VELOCITY_RESET_METERS_PER_SECOND = 0.0;
+
     // TODO make a read-only version of ModeState to disallow registering mode switches in an InputProcessor, outside of SubsystemInputProcessor
     public ElevatorInputProcessor(final Elevator elevator, final CommandXboxController driver, final ModeState<ElevatorMode> state, Function<ModeState<?>, BooleanSupplier> isModeActive) {
         super(isModeActive);
@@ -33,12 +54,24 @@ public class ElevatorInputProcessor extends InputProcessor {
         this.elevator = elevator;
         this.driver = driver;
         this.state = state;
+
+        // modules
+        this.defaultParams = new JoystickModuleParams(elevator, isModeActive.apply(state), state.noSwitchesActive(), state.is(ElevatorMode.DEBUG), JOYSTICK_DEADBAND);
+
+        // - both
+        this.positionModule = new JoystickModule(defaultParams, new ControlModule(value -> elevator.updateSetpoint(Meters.of(value)), BUTTON_POSITION_RESET_METERS));
+        this.velocityModule = new JoystickModule(defaultParams, new ControlModule(value -> elevator.updateSetpoint(MetersPerSecond.of(value)), BUTTON_VELOCITY_RESET_METERS_PER_SECOND));
+        this.voltageModule = new JoystickModule(defaultParams, new ControlModule(value -> elevator.updateVoltage(Volts.of(value)), BUTTON_VELOCITY_RESET_VOLTS));
     }
 
     @Override
     public void configureTriggers() {
-        // buttons
-        // state-based
+        positionModule.configureSetValueButton(driver.x());
+        velocityModule.configureSetValueButton(driver.y());
+        voltageModule.configureSetValueButton(driver.a());
+
+        // resetting voltage to zero functions as a reset for all three modules
+        voltageModule.configureResetButton(driver.b());
     }
 
     @Override
@@ -48,7 +81,8 @@ public class ElevatorInputProcessor extends InputProcessor {
         Map<ModeState<?>, Command> commands = defaults.get(elevator);
 
         commands.put(state, state.selectRunnable(Map.of(
-            ElevatorMode.MANUAL, this::driveManual
+            ElevatorMode.MANUAL, this::driveManual,
+            ElevatorMode.DEBUG, this::driveViaModules
         ), elevator));
     }
 
@@ -67,6 +101,25 @@ public class ElevatorInputProcessor extends InputProcessor {
 
         // drive
         elevator.updateSetpoint(verticalSpeed);
+    }
+
+    public void driveViaModules() {
+        boolean leftBumperDown = driver.leftBumper().getAsBoolean();
+        boolean rightBumperDown = driver.rightBumper().getAsBoolean();
+
+        // positive, by default, means downwards, so we're inverting it to make upwards positive
+        double value = -driver.getRightY();
+
+        if (leftBumperDown && rightBumperDown) {
+            // value is interpreted as radians, after range shifting
+            positionModule.driveJoystick(value);
+        } else if (leftBumperDown && !rightBumperDown) {
+            // value is interpreted as radians/s, after range shifting
+            velocityModule.driveJoystick(value);
+        } else if (!leftBumperDown && rightBumperDown) {
+            // value is interpreted as volts, after range shifting
+            voltageModule.driveJoystick(value);
+        }
     }
 
     
