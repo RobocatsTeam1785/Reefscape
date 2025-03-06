0cb667abe49aed277d5082b587ef22d00fc2dce7
Simon Beebe <152335637+Orbixel@users.noreply.github.com>
2025-03-05 19:46:57 -0600

    done
    this is just a auto that will go back out of the starting line

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,10 +4,25 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.photonvision.EstimatedRobotPose;
+
+import com.studica.frc.AHRS;
+
 import edu.wpi.first.epilogue.Epilogue;
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.Swerve;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -18,6 +33,9 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   // pass the period so the drive subsystem can discretize the chassis speeds with it
   public RobotContainer container;
+  private Swerve swerve;
+  private LinearVelocity xSpeed, ySpeed;
+  private AngularVelocity angVel;
   // public SysIdBot bot;
 
   /**
@@ -25,7 +43,17 @@ public class Robot extends TimedRobot {
    * initialization code.
    */
   public Robot() {
+    
     container = new RobotContainer(getPeriod());
+    //robot doesn't initialize the pose this early on and since pose is just needed i put this as a null value
+    swerve = new Swerve(kDefaultPeriod,null);
+
+
+    
+    
+
+
+    
     // bot = new SysIdBot(getPeriod());
 
     Epilogue.bind(this);
@@ -39,7 +67,13 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    //x is set instead of y axis because of robots pose 
+    xSpeed = MetersPerSecond.of(-10);
+    ySpeed = MetersPerSecond.of(0);
+    angVel = RadiansPerSecond.of(0);
+    swerve.driveRobotRelative(xSpeed,ySpeed,angVel);
+  }
 
   @Override
   public void autonomousPeriodic() {}
