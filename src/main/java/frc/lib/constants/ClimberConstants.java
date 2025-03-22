a14b448fabcb4181052edb3a09779350867f2ecb
Nyla Fluharty <nfluharty@proton.me>
2025-03-22 11:33:05 -0500

    fix(climber): update CAN id to match robot

--- src/main/java/frc/lib/constants/ClimberConstants.java
+++ src/main/java/frc/lib/constants/ClimberConstants.java
@@ -6,7 +6,7 @@ import edu.wpi.first.units.measure.Current;
 
 public class ClimberConstants {
     // TODO set in Rev Hardware Client
-    public static final int MOTOR_ID = 1;
+    public static final int MOTOR_ID = 21;
 
     public static final Current SMART_CURRENT_LIMIT = Amps.of(40);
 }
