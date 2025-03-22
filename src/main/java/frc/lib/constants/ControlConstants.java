4d5db102c0eebc98dcc104f545edcbf97d4f539e
Nyla Fluharty <nfluharty@proton.me>
2025-03-22 11:56:33 -0500

    fix(control): change climber control voltage range to [-12V, 12V] to enable functional climbing

--- src/main/java/frc/lib/constants/ControlConstants.java
+++ src/main/java/frc/lib/constants/ControlConstants.java
@@ -4,6 +4,6 @@ public class ControlConstants {
     // climber
     public static final double
         CLIMBER_VOLTAGE_CONTROL_DEADBAND = 0.1,
-        CLIMBER_VOLTAGE_CONTROL_POSITIVE_FACTOR = 1.0,
-        CLIMBER_VOLTAGE_CONTROL_NEGATIVE_FACTOR = 1.0;
+        CLIMBER_VOLTAGE_CONTROL_POSITIVE_FACTOR = 12.0,
+        CLIMBER_VOLTAGE_CONTROL_NEGATIVE_FACTOR = 12.0;
 }
